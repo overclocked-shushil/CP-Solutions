42502ee962fd2abbcc98a6f1afa80eb921ac5f07
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-23 12:50:59 +0530

    Add Koko-Eating-Bananas (binary-search)

---
+++ leetcode/binary-search/875-Koko-Eating-Bananas.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long hours(vector<int>& piles, int mid) {
+        long long total = 0;
+
+        for (int i = 0; i < piles.size(); i++) {
+            total += (piles[i] + mid - 1) / mid;
+        }
+        return total;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int max_val = *max_element(piles.begin(), piles.end());
+        int low = 1;
+        int high = max_val;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (hours(piles,mid) <= h) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+};
