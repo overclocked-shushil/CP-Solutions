ccae1d2bbf046ac59305ff77cf3cd15da9452868
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-26 01:05:13 +0530

    Add Maximum-Subarray (dynamic-programming)

---
+++ leetcode/dynamic-programming/53-Maximum-Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maximum = INT_MIN;
+        int sum = 0;
+        for(int i = 0;i<nums.size();i++){
+            sum = sum+nums[i];
+            if (sum > maximum) maximum = sum;
+            if (sum < 0) sum = 0;
+        }
+       return maximum; 
+    }
+};
