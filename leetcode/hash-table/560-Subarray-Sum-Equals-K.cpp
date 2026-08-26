0b6560019e4cf43d8c85dbf2a16a92b930da3554
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-26 11:31:41 +0530

    Add Subarray-Sum-Equals-K (hash-table)

---
+++ leetcode/hash-table/560-Subarray-Sum-Equals-K.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map <int , int>mpp;
+        mpp[0] =1;
+        int prefixsum = 0;
+        int count = 0;
+        for (int i = 0; i<nums.size();i++){
+            prefixsum +=nums[i];
+            int remove = prefixsum - k;
+            count += mpp[remove];
+            mpp[prefixsum]+=1;
+        }
+        return count;   
+    }
+};
