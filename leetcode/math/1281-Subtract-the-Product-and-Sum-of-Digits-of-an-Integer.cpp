a55f5319a933025b4e1a9ef2ff565e6999caa616
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-26 11:36:21 +0530

    Add Subtract-the-Product-and-Sum-of-Digits-of-an-Integer (math)

---
+++ leetcode/math/1281-Subtract-the-Product-and-Sum-of-Digits-of-an-Integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int prod = 1;
+        int sum = 0;
+        while(n !=0){
+            int ld = n %10;
+            prod = prod*ld;
+            sum = sum+ld;
+            n = n/10;
+        }
+
+       return prod-sum; 
+    }
+};
