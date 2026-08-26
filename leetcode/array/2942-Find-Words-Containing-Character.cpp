0b07ad41005132e6b69fc01a2fb0b44cc361199c
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-26 11:49:37 +0530

    Add Find-Words-Containing-Character (array)

---
+++ leetcode/array/2942-Find-Words-Containing-Character.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector <int> index;
+        for (int i = 0; i< words.size();i++){
+            if (words[i].find(x) != -1){
+                index.push_back(i);
+            }
+        }
+        return index;
+        
+    }
+};
