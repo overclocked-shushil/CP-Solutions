205e3dc7dc53fc533fdc026baffe814ea7b3a08c
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-26 23:00:17 +0530

    Add Find-the-Smallest-Divisor-Given-a-Threshold (binary-search)

---
+++ leetcode/binary-search/1283-Find-the-Smallest-Divisor-Given-a-Threshold.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool divide(vector<int>& nums, int mid, int threshold) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += (nums[i] + mid - 1) / mid;
+        }
+        if (sum <= threshold)return true;
+        else return false;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int maxi = *max_element(nums.begin(), nums.end());
+        int low = 1;
+        int high = maxi;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (divide(nums, mid, threshold)) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
