61f568735a531c3e9eb02e675770861081652d10
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-26 11:45:28 +0530

    Add First-Letter-to-Appear-Twice (hash-table)

---
+++ leetcode/hash-table/2351-First-Letter-to-Appear-Twice.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_set<char> seen;
+        
+        for (int i  = 0 ;i<s.size();i++){
+            if (seen.count(s[i])) return s[i];
+            seen.insert(s[i]);
+        }
+      return ' ';  
+    }
+};
