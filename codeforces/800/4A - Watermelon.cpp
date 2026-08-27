6221634cbca6583b63955c0aeeb828065d8a069d
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-27 13:28:01 +0530

    Add Watermelon (800)

---
+++ codeforces/800/4A - Watermelon.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        int a;
+        cin >> a ;
+        
+    if (a % 2 == 0 && a!= 2){
+        cout << "YES" << endl;
+    }
+else {
+    cout << "NO" << endl;
+}
+
+}
+return 0;
+}
