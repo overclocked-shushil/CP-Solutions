96c027e171c489b8d31dfde02893db4be5e07434
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-28 00:07:24 +0530

    Add Sum-of-Number-and-Its-Reverse (math)

--- leetcode/math/2443-Sum-of-Number-and-Its-Reverse.cpp
+++ leetcode/math/2443-Sum-of-Number-and-Its-Reverse.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int rev(int n ){
+    int rev(int n) {
         int revnum = 0;
-        while (n!=0){
-            int ld = n%10;
-            revnum = (revnum*10)+ld;
-            n = n/10;
+        while (n != 0) {
+            int ld = n % 10;
+            revnum = (revnum * 10) + ld;
+            n = n / 10;
         }
         return revnum;
     }
     bool sumOfNumberAndReverse(int num) {
-        for (int i = num/2 ; i<=num;i++){
-            if ( i + rev(i) == num) return true;
+        for (int i = num / 2; i <= num; i++) {
+            if (i + rev(i) == num)
+                return true;
         }
         return false;
     }
