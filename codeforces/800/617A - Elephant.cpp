5a402f5f11531d05c5e0a159bddb69b247345d81
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-08-29 13:51:19 +0530

    Add Elephant (800)

---
+++ codeforces/800/617A - Elephant.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+        int sum = (n+4)/5;
+       cout << sum << endl;
+     
+    return 0;
+}
