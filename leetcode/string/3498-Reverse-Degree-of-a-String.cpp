6cafd88a466b338d3374ddc2db15c40e6c53e7a9
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-01 02:44:21 +0530

    Add Reverse-Degree-of-a-String (string)

--- leetcode/string/3498-Reverse-Degree-of-a-String.cpp
+++ leetcode/string/3498-Reverse-Degree-of-a-String.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int reverseDegree(string s) {
         int sum = 0;
-        for (int i = 0; i<s.size();i++){
+        for (int i = 0; i < s.size(); i++) {
             int rev = 26 - (s[i] - 'a' + 1) + 1;
-            cout << rev << endl;
-            cout << i+1 << endl;
-            sum += (rev * (i+1));
+            sum += (rev * (i + 1));
         }
-        
         return sum;
     }
 };
