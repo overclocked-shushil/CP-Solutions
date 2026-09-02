b1b74f51e88d78d8e950c47115cb4a6a00cfc47d
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-02 23:11:23 +0530

    Add Odd Eraser (Unrated)

--- codeforces/Unrated/2258A - Odd Eraser.cpp
+++ codeforces/Unrated/2258A - Odd Eraser.cpp
@@ -2,32 +2,21 @@
 #include <vector>
 #include <string>
 #include <numeric>
- 
 using namespace std;
- 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
  
    int t;
- 
     cin >> t;
- 
     while (t--) {
- 
         int n;
- 
         cin >> n;
- 
         vector<long long> a(n);
- 
         for (int i = 0; i < n; i++) {
- 
             cin >> a[i];
  
         }
- 
         cout << gcd(a[0], a[n - 1]) << '\n';  
 }
-    
 }
