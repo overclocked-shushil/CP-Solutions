3a69bddec3a310f6834735a3988ccf36cd897cf4
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-02 21:35:48 +0530

    Add Append-Characters-to-String-to-Make-Subsequence (two-pointers)

---
+++ leetcode/two-pointers/2486-Append-Characters-to-String-to-Make-Subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while (s.size()>i && t.size() > j){
+            if (s[i] == t[j]){
+                i++;
+                j++;
+            }
+            else i++;
+        }
+        return t.size() - j;        
+    }
+};
