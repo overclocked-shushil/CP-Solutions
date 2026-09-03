42ebde7242e12e9ee3a57e3cb59dc734f70ef3d2
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-03 16:50:33 +0530

    Add Construct-Uniform-Parity-Array-II (array)

--- leetcode/array/3876-Construct-Uniform-Parity-Array-II.cpp
+++ leetcode/array/3876-Construct-Uniform-Parity-Array-II.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int mini = *min_element(nums1.begin(),nums1.end());
-        if (mini % 2 == 1) return true;
+        if (mini % 2 != 0) return true;
         for (int n: nums1){
-            if (n % 2 == 1) return false;
+            if (n % 2 != 0) return false;
         }
         return true;
     }
