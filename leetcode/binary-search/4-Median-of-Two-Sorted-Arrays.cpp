5ba814d2d5c916074894cd730e1ab3ff0219d776
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-04 17:46:36 +0530

    Add Median-of-Two-Sorted-Arrays (binary-search)

---
+++ leetcode/binary-search/4-Median-of-Two-Sorted-Arrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());
+        int n = nums1.size();
+        int even = (n / 2) + (n + 1 / 2.0);
+        if (n % 2 != 0) return nums1[n / 2];
+
+        else return (nums1[n / 2 - 1] + nums1[n / 2]) / 2.0;
+    }
+};
