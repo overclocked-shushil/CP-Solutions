33129b70f8683d74739f1f20b23a9924bfda4f5f
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-06 15:46:44 +0530

    Add Largest-3-Same-Digit-Number-in-String (string)

--- leetcode/string/2264-Largest-3-Same-Digit-Number-in-String.cpp
+++ leetcode/string/2264-Largest-3-Same-Digit-Number-in-String.cpp
@@ -4,7 +4,6 @@ public:
         vector <string> n = {"999","888","777","666","555","444","333","222","111","000"};
         for (int i = 0;i<10;i++){
             if (num.find(n[i]) != string::npos) return n[i];
-           
         }
         return "";
     }
