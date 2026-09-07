1dece704310edb78242474f905013b472b34eeff
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-07 19:08:08 +0530

    Add Rotate-String (string)

--- leetcode/string/796-Rotate-String.cpp
+++ leetcode/string/796-Rotate-String.cpp
@@ -3,7 +3,6 @@ public:
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) return false;
         string ans = s+s;
-        int n = ans.size()-1;
         if (ans.find(goal)!= std::string::npos) return true;
         else return false;
         
