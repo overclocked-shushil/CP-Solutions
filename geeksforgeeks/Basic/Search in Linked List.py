5d2b7bfda756678d72e0f9339a4d3c8ce96b94d0
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-17 01:11:48 +0530

    Add Search in Linked List (Basic)

---
+++ geeksforgeeks/Basic/Search in Linked List.py
@@ -0,0 +1,23 @@
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    bool searchKey(Node* head, int key) {
+        Node* temp = head;
+        while (temp != NULL){
+            if (temp ->data == key) return true;
+            temp  = temp -> next;
+        }
+       return false; 
+    }
+};
