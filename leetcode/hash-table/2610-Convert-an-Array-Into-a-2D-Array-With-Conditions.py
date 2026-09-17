1d4e646530c4ce21a131638486fbea0072458486
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-17 19:03:01 +0530

    Add Convert-an-Array-Into-a-2D-Array-With-Conditions (hash-table)

---
+++ leetcode/hash-table/2610-Convert-an-Array-Into-a-2D-Array-With-Conditions.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMatrix(self, nums):
+        frequency = [0] * (len(nums) + 1)
+        res = []
+
+        for i in nums:
+            if frequency[i] >= len(res):
+                res.append([])
+            res[frequency[i]].append(i)
+            frequency[i] += 1
+
+        return res
