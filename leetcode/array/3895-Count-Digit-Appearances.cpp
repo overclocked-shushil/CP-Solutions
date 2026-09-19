44c1130c793901a2bbbc9fc33047c95b0e521d88
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-19 17:01:05 +0530

    Add Count-Digit-Appearances (array)

--- leetcode/array/3895-Count-Digit-Appearances.cpp
+++ leetcode/array/3895-Count-Digit-Appearances.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int countDigitOccurrences(vector<int>& nums, int digit) {
-        string store = "";
-        for (int n : nums){
-            store+= to_string(n);
-        }
         int count = 0;
-        for (char c : store){
-            if (c == (digit + '0')) count++;
+        for (int n : nums) {
+            string s = to_string(n);
+            for (char c : s) {
+                if (c == digit + '0') {
+                    count++;
+                }
+            }
         }
         return count;
     }
