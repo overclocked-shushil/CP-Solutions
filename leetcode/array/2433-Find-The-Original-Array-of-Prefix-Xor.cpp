fee47af1e2fb97e130e51df2c28caf1c67bf7db9
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-24 01:48:14 +0530

    Add Find-The-Original-Array-of-Prefix-Xor (array)

--- leetcode/array/2433-Find-The-Original-Array-of-Prefix-Xor.cpp
+++ leetcode/array/2433-Find-The-Original-Array-of-Prefix-Xor.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
-        vector<int> ans;
-        ans.push_back(pref[0]);
-        for (int i = 0; i < pref.size() - 1; i++) {
-            ans.push_back(pref[i] ^ pref[i + 1]);
+        int n = pref.size();
+        for (int i = n - 1; i > 0; i--) {
+            pref[i] = pref[i] ^ pref[i - 1];
         }
-        return ans;
+        return pref;
     }
 };
