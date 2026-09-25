4fb286059c8273be0468e33a7161ac0526f41bce
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-26 03:10:13 +0530

    Add Turn Into a Palindrome (Unrated)

---
+++ codeforces/Unrated/2267A - Turn Into a Palindrome.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        char c;
+        cin >> n >> c;
+        string s;
+        cin >> s;
+        int ans = 0;
+        for (int i = 0; i < n / 2; i++) {
+            int j = n - 1 - i;
+            if (s[i] == s[j]) {
+                continue;
+            }
+            else if (s[i] == c || s[j] == c) {
+                ans += 1;
+            }
+            else {
+                ans += 2;
+            }
+        }
+ 
+        cout << ans << '\n';
+    }
+    return 0;
+}
