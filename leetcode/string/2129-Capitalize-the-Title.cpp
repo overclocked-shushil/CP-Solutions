0da49b4a454eea9617f5257d227ce442a2a1f1c7
PAL SHUSHIL NANDLAL <palshushilnandlal@gmail.com>
2026-09-26 01:54:14 +0530

    Add Capitalize-the-Title (string)

---
+++ leetcode/string/2129-Capitalize-the-Title.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string capitalizeTitle(string s) {
+        int n = s.size();
+        for (auto &c :s) c = tolower(c);
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || s[i - 1] == ' ') {
+                int j = i;
+
+                while (j < n && s[j] != ' ')
+                    j++;
+
+                int len = j - i;
+
+                if (len >= 3) {
+                    s[i] = toupper(s[i]);
+                }
+
+                i = j - 1;
+            }
+        }
+
+        return s;
+    }
+};
